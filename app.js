1a726118cec6542ac55666aa5b64a3eb87242bae
doudouleydi_fall <dfall.prestataire@oui.sncf>
2021-11-03 17:20:30 +0100

    Review code

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const userService = require('./services/user-service');
 const hotelService = require('./services/hotel-service');
-const helper = require('./services/helper');
 
 function findHotelsNearby(lat, lng, radius) {
     // TODO implement me
