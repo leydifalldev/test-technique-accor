0b1245a4c7648d6ad0d039a13f6d3c212bb2e73d
doudouleydi_fall <dfall.prestataire@oui.sncf>
2021-11-03 17:18:06 +0100

    Review code

--- app.js
+++ app.js
@@ -1,49 +1,22 @@
 const userService = require('./services/user-service');
 const hotelService = require('./services/hotel-service');
-const priceService = require('./services/price-service');
 const helper = require('./services/helper');
-
-function getHotelsByDistance(hotels, lat, lng, radius) {
-    return hotels.map((hotel) => ({
-        ...hotel,
-        distance: helper.distance(lat, lng, hotel.latitude, hotel.longitude)/1000,
-    })).filter((hotel) => hotel.distance <= radius)
-}
 
 function findHotelsNearby(lat, lng, radius) {
     // TODO implement me
-    return getHotelsByDistance(hotelService.getHotels(), lat, lng, radius);
+    return hotelService.findHotelsByDistance(hotelService.getHotels(), lat, lng, radius);
 }
 
 function findHotelNearbyWithBestOffer(lat, lng, radius, date) {
     // TODO implement me
-    let hotels = priceService.getPrices()
-    .map((hotelOffers) => {
-        // fusion each hotel and his own offers hotel + offers
-        return { 
-            ...hotelService.getHotels().find((hotel) => hotel.ridCode === hotelOffers.ridCode),
-            // filter offers by date and fare for each hotel
-            offer: hotelOffers.offers
-                .filter((offer) => (offer.fare === "STANDARD" && offer.date === date))
-                .sort((a, b) => (a.price - b.price))[0],
-        }
-    });
+    let hotels = [];
+    // merge each hotel with his the cheapest offer with the filter by date
+    hotels = hotelService.getHotelWithOffersByDate(date)
     // filter hotel by distance
-    hotels = getHotelsByDistance(hotels, lat, lng, radius)
-    .filter((hotel) => (hotel.distance <= radius && hotel.offer))
-    .sort((a, b) => {
-        if (a.offer.price < b.offer.price) {
-            return -1;
-        }
-        if (a.offer.price > b.offer.price) {
-            return 1;
-        }
-        if (a.offer.price === b.offer.price) {
-            return (a.distance > b.distance) ? 1 : -1;
-        }
-        return 0;
-    })
-
+    hotels = hotelService.filterHotelByDistance(hotels, lat, lng, radius)
+    // sort offer by price
+    hotels = hotelService.sortOfferByPrice(hotels)
+    
     return hotels;
 }
 
@@ -57,4 +30,4 @@ const hotels = findHotelNearbyWithBestOffer(48.838385, 2.290459, 2, "12/01/2021"
 console.log("hotels", hotels)
 console.log("hotels.length", hotels.length)
 
-document.writeln("I am in grade " + hotels);
+// document.writeln("I am in grade " + hotels);
--- services/hotel-service.js
+++ services/hotel-service.js
@@ -1,9 +1,56 @@
+const priceService = require('./price-service');
 const HOTELS = require('./data/hotels.json').hotels;
+const helper = require('./helper');
 
 const getHotels = () => {
 	return HOTELS;
 }
 
+function findHotelsByDistance(hotels=[], lat, lng, radius) {
+    return hotels.map((hotel) => ({
+        ...hotel,
+        distance: helper.distance(lat, lng, hotel.latitude, hotel.longitude)/1000,
+    })).filter((hotel) => hotel.distance <= radius)
+}
+
+function getHotelWithOffersByDate(date) {
+	return priceService.getPrices()
+    .map((hotelOffers) => {
+        // merge each hotel with his cheapest offer
+        return {
+            ...getHotels().find((hotel) => hotel.ridCode === hotelOffers.ridCode),
+            // filter offers by date and fare for each hotel
+            offer: hotelOffers.offers
+                .filter((offer) => (offer.fare === "STANDARD" && offer.date === date))
+                .sort((a, b) => (a.price - b.price))[0],
+        }
+    });
+}
+
+function filterHotelByDistance(hotels, lat, lng, radius) {
+	return findHotelsByDistance(hotels, lat, lng, radius)
+    .filter((hotel) => (hotel.offer && hotel.distance <= radius))
+}
+
+function sortOfferByPrice(hotels=[]) {
+	return hotels.sort((a, b) => {
+        if (a.offer.price < b.offer.price) {
+            return -1;
+        }
+        if (a.offer.price > b.offer.price) {
+            return 1;
+        }
+        if (a.offer.price === b.offer.price) {
+            return (a.distance > b.distance) ? 1 : -1;
+        }
+        return 0;
+    })
+}
+
 module.exports = {
-	getHotels: getHotels
+	getHotels: getHotels,
+	findHotelsByDistance: findHotelsByDistance,
+	getHotelWithOffersByDate: getHotelWithOffersByDate,
+	filterHotelByDistance: filterHotelByDistance,
+	sortOfferByPrice: sortOfferByPrice,
 }
